48d9db42876c6e358e00232c638678ed0439f394
Greg Pratt <Greg.Pratt@bjss.com>
2019-01-16 13:23:18 +0000

    fix(perf): remove pre-load and fix toggle on home page

--- src/components/home/home-page.tsx
+++ src/components/home/home-page.tsx
@@ -10,7 +10,7 @@ export class HomePage {
     return (
       <div class="o-home-page u-centered u-absolute-center">
         <h1 class="c-heading u-super">Togglz</h1>
-        <blaze-toggle class="u-super" type="brand" />
+        <blaze-toggle class="u-super" type="brand" toggled />
         <h2 class="c-heading u-medium u-text--quiet c-tagline">Feature toggle service for websites and apps</h2>
       </div>
     );

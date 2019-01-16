e425e04587e3933edd058bdb6fc576d2e20489d0
Greg Pratt <Greg.Pratt@bjss.com>
2019-01-16 13:49:34 +0000

    style(tweaks): theme colour and home page font size

--- src/components/home/home-page.tsx
+++ src/components/home/home-page.tsx
@@ -8,7 +8,7 @@ import '@stencil/router';
 export class HomePage {
   render() {
     return (
-      <div class="o-home-page u-centered u-absolute-center">
+      <div class="o-home-page u-centered u-absolute-center u-large">
         <h1 class="c-heading u-super">Togglz</h1>
         <blaze-toggle class="u-super" type="brand" toggled />
         <h2 class="c-heading u-medium u-text--quiet c-tagline">Feature toggle service for websites and apps</h2>
